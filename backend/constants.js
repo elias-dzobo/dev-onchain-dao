c26d2867e48f6a42b57087b8404ac2c4507473f6
elias-dzobo <elias.dzobo@gmail.com>
2023-06-09 20:43:23 +0000

    initial commit

---
+++ backend/constants.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x02cFBFD55b9d14877629470C69a5Df7E575F1659";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
